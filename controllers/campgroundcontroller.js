c3ce93a7d6cfbc90d8123432dc70e47411c51524
maimengmeo <mai291294@gmail.com>
2023-04-27 15:43:13 -0400

    refactored to campground controller

---
+++ controllers/campgroundcontroller.js
@@ -0,0 +1,74 @@
+const Campground = require("../models/campground");
+
+module.exports.index = async (req, res) => {
+    const campgrounds = await Campground.find({});
+    res.render("campgrounds/index", { campgrounds });
+};
+
+module.exports.newCampgroundForm = (req, res) => {
+    res.render("campgrounds/new");
+};
+
+module.exports.createCampground = async (req, res, next) => {
+    //if (!req.body.campground) throw new ExpressError("Invalid Campground Data", 400);
+    const campground = new Campground(req.body.campground); //create new campground from req.body
+    campground.author = req.user._id;
+    await campground.save();
+
+    req.flash("success", "Successfully add a new campground!");
+    res.redirect(`/campgrounds/${campground._id}`); //redirect to the new campground page after created
+};
+
+module.exports.showCampground = async (req, res) => {
+    const campground = await Campground.findById(req.params.id)
+        .populate({
+            path: "reviews",
+            populate: { path: "author" },
+        })
+        .populate("author");
+
+    if (!campground) {
+        req.flash("error", "Cannot find that campground");
+        return res.redirect("/campgrounds");
+    }
+
+    req.session.returnTo = req.originalUrl;
+    res.render("campgrounds/show", { campground });
+
+    //get the id from index.ejs, find campground by id, put it to show.ejs
+};
+
+module.exports.editCampgroundForm = async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findById(id);
+
+    if (!campground) {
+        req.flash("error", "Cannot find that campground");
+        return res.redirect("/campgrounds");
+    }
+
+    res.render("campgrounds/edit", { campground });
+
+    //get the selected to edit campground from id from url, inject it to edit page
+};
+
+module.exports.updateCampground = async (req, res) => {
+    //obj destructuring, extract id
+    const { id } = req.params;
+
+    const campground = await Campground.findByIdAndUpdate(id, {
+        ...req.body.campground,
+        //need to use spread obj here bc this method takes an obj w new data
+        //need to spread the obj into obj literal to create new obj
+        //that contain all original properties along w new data submitted
+    });
+    req.flash("success", "Successfully updated campground");
+    res.redirect(`/campgrounds/${campground._id}`);
+};
+
+module.exports.deleteCampground = async (req, res) => {
+    const { id } = req.params;
+    await Campground.findByIdAndDelete(id);
+    req.flash("success", "Successfully deleted campground");
+    res.redirect("/campgrounds");
+};
--- routes/campgroundRoutes.js
+++ routes/campgroundRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-
+const campgroundController = require("../controllers/campgroundcontroller");
 const catchAsync = require("../utils/catchAsync");
 const Campground = require("../models/campground");
 
@@ -13,57 +13,24 @@ const {
 
 //path - order is matter========================================
 //display all campgrounds
-router.get(
-    "/",
-    catchAsync(async (req, res) => {
-        const campgrounds = await Campground.find({});
-        res.render("campgrounds/index", { campgrounds });
-    })
-);
+router.get("/", catchAsync(campgroundController.index));
 
 //go to create campground page
-router.get("/new", isLoggedIn, (req, res) => {
-    res.render("campgrounds/new");
-});
+router.get("/new", isLoggedIn, campgroundController.newCampgroundForm);
 
 //create campground
 router.post(
     "/",
     validateCampground,
     isLoggedIn,
-    catchAsync(async (req, res, next) => {
-        //if (!req.body.campground) throw new ExpressError("Invalid Campground Data", 400);
-        const campground = new Campground(req.body.campground); //create new campground from req.body
-        campground.author = req.user._id;
-        await campground.save();
-
-        req.flash("success", "Successfully add a new campground!");
-        res.redirect(`/campgrounds/${campground._id}`); //redirect to the new campground page after created
-    })
+    catchAsync(campgroundController.createCampground)
 );
 
 //go to selected campground page
 router.get(
     "/:id",
     storeReturnTo,
-    catchAsync(async (req, res) => {
-        const campground = await Campground.findById(req.params.id)
-            .populate({
-                path: "reviews",
-                populate: { path: "author" },
-            })
-            .populate("author");
-
-        if (!campground) {
-            req.flash("error", "Cannot find that campground");
-            return res.redirect("/campgrounds");
-        }
-
-        req.session.returnTo = req.originalUrl;
-        res.render("campgrounds/show", { campground });
-
-        //get the id from index.ejs, find campground by id, put it to show.ejs
-    })
+    catchAsync(campgroundController.showCampground)
 );
 
 //go to edit page
@@ -71,19 +38,7 @@ router.get(
     "/:id/edit",
     isLoggedIn,
     isAuthor,
-    catchAsync(async (req, res) => {
-        const { id } = req.params;
-        const campground = await Campground.findById(id);
-
-        if (!campground) {
-            req.flash("error", "Cannot find that campground");
-            return res.redirect("/campgrounds");
-        }
-
-        res.render("campgrounds/edit", { campground });
-
-        //get the selected to edit campground from id from url, inject it to edit page
-    })
+    catchAsync(campgroundController.editCampgroundForm)
 );
 
 //app.put is used to update resource at the spec path
@@ -92,31 +47,14 @@ router.put(
     validateCampground,
     isLoggedIn,
     isAuthor,
-    catchAsync(async (req, res) => {
-        //obj destructuring, extract id
-        const { id } = req.params;
-
-        const campground = await Campground.findByIdAndUpdate(id, {
-            ...req.body.campground,
-            //need to use spread obj here bc this method takes an obj w new data
-            //need to spread the obj into obj literal to create new obj
-            //that contain all original properties along w new data submitted
-        });
-        req.flash("success", "Successfully updated campground");
-        res.redirect(`/campgrounds/${campground._id}`);
-    })
+    catchAsync(campgroundController.updateCampground)
 );
 
 router.delete(
     "/:id",
     isLoggedIn,
     isAuthor,
-    catchAsync(async (req, res) => {
-        const { id } = req.params;
-        await Campground.findByIdAndDelete(id);
-        req.flash("success", "Successfully deleted campground");
-        res.redirect("/campgrounds");
-    })
+    catchAsync(campgroundController.deleteCampground)
 );
 
 module.exports = router;
