c1533f3eb4bda6532568baff939ef2b4384a130f
maimengmeo <mai291294@gmail.com>
2023-04-26 20:32:46 -0400

    updated post register

--- models/user.js
+++ models/user.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 const passportLocalMongoose = require("passport-local-mongoose");
 
 const UserSchema = new Schema({
-    emai: {
+    email: {
         type: String,
         required: true,
         unique: true,
