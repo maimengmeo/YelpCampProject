aea426990808577b899f0c3d2f87f69dfa2fbada
maimengmeo <mai291294@gmail.com>
2023-04-27 16:24:31 -0400

    refactored code

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -8,22 +8,23 @@ const userController = require("../controllers/userController");
 
 const { storeReturnTo } = require("../middleware");
 
-router.get("/register", userController.registerForm);
+router
+    .route("/register")
+    .get(userController.registerForm)
+    .post(catchAsync(userController.userRegister));
 
-router.post("/register", catchAsync(userController.userRegister));
-
-router.get("/login", userController.loginForm);
-
-router.post(
-    "/login",
-    storeReturnTo, //to save the returnTo value from session to res.locals
-    passport.authenticate("local", {
-        //authenticate user before login, if fail, display flash message and redirect to login page
-        failureFlash: true,
-        failureRedirect: "/login",
-    }),
-    userController.userLogin
-);
+router
+    .route("/login")
+    .get(userController.loginForm)
+    .post(
+        storeReturnTo, //to save the returnTo value from session to res.locals
+        passport.authenticate("local", {
+            //authenticate user before login, if fail, display flash message and redirect to login page
+            failureFlash: true,
+            failureRedirect: "/login",
+        }),
+        userController.userLogin
+    );
 
 router.get("/logout", userController.logout);
 
