5146a1ae09d639e310b53bdfff1efa4ed3641b08
maimengmeo <mai291294@gmail.com>
2023-05-01 17:10:37 -0400

    added popup for map

--- public/scripts/showPageMap.js
+++ public/scripts/showPageMap.js
@@ -9,4 +9,12 @@ const map = new mapboxgl.Map({
 //make the marker
 const marker = new mapboxgl.Marker()
     .setLngLat(campground.geometry.coordinates)
+    .setPopup(
+        new mapboxgl.Popup({ offset: 25 }).setHTML(
+            `
+                <h5>${campground.title}</h5>
+                <p>${campground.location}</p>
+            `
+        )
+    )
     .addTo(map);
